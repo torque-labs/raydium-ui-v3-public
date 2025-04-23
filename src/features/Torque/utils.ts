bbb95feffd37d0ba398decdac0f2940dab3547bd
nardevvv <vedran.sisic+personal@gmail.com>
2025-04-23 18:49:50 -0400

    Revert to devnet as default

--- src/features/Torque/utils.ts
+++ src/features/Torque/utils.ts
@@ -3,7 +3,7 @@ import { TorqueConversion, TorqueRawOffer } from './types'
 /**
  * Torque API URL
  */
-const TORQUE_API_URL = process.env.NEXT_PUBLIC_TORQUE_API_URL || 'https://server.torque.so'
+const TORQUE_API_URL = process.env.NEXT_PUBLIC_TORQUE_API_URL || 'https://server-devnet.torque.so'
 
 /**
  * Torque API routes
